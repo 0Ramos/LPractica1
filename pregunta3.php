baa4918b18cab13633138403ddc59c5c301e31be
0Ramos <66374859+0Ramos@users.noreply.github.com>
2020-11-23 09:36:04 -0500

    Pregunta3

---
+++ pregunta3.php
@@ -0,0 +1,14 @@
+
+
+<?php
+for ($x=12; $x<=6; $x++)
+{
+    for ($y=12; $y<=6; $y++){
+        $multiplicacion =$x*$y;
+        echo"<td>";
+        echo $multiplicacion;
+        echo "</td>";
+
+}
+    echo "</tr>";
+}
